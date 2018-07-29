47a7bcb96238fa0089a7da92d830913a7f611446
RobDrury <rdrury1513@hotmail.com>
2018-07-29 19:54:05 -0400

    fix heroku lanuch errors

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5433/wow_arena_tracker_dev'
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db',MigrateCommand)
+port = int(os.environ.get('PORT', 5000))
 
 
 bcrypt = Bcrypt(app)
@@ -23,7 +25,9 @@ from route import *
 if __name__ == '__main__':
     app.secret_key = "top_secret"
    # db.create_all()
-    app.run()
     manager.run()
+    app.run(host='0.0.0.0', port=port)
+
+#import os from pml import app port = int(os.environ.get('PORT', 5000)) app.run(host='0.0.0.0', port=port)
 
 
